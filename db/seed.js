d6303a5aa5083acbd4a06ae3fbccca78cfa2912e
Liz <lizkristinaphillips@gmail.com>
2017-03-10 17:22:25 -0500

    corrected latitude and longitude swap in seed file

--- db/seed.js
+++ db/seed.js
@@ -6,9 +6,9 @@ const db = require('APP/db')
 // ], user => db.model('users').create(user))
 
 const seedSecrets = () => db.Promise.map([
-  {text: 'this is a secret at GHA', latitude: 40.704887199999995, longitude: -74.0123736},
-  {text: 'this is another secret at Open Market', latitude: 40.705343, longitude: -74.010152},
-  {text: 'this is a secret at Trinity Church', latitude: 40.707819, longitude: -74.011770},
+  {text: 'this is a secret at GHA', latitude: -74.0123736, longitude: 40.704887199999995},
+  {text: 'this is another secret at Open Market', latitude: -74.010152, longitude: 40.705343},
+  {text: 'this is a secret at Trinity Church', latitude: -74.011770, longitude: 40.707819},
 ], secret => db.model('secrets').create(secret))
 
 db.didSync
