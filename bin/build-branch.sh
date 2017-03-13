78728e07795ac55e707014c8fd72c741a46dc6a5
Liz <lizkristinaphillips@gmail.com>
2017-03-13 12:58:13 -0400

    changes name to deadrop. changes build-branch.sh to work without '[['

--- bin/build-branch.sh
+++ bin/build-branch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Paths to add to the deployment branch.
 #
 # These paths will be added with git add -f, to include build artifacts
@@ -16,7 +15,7 @@ echoed() {
     $*
 }
 
-if [[ $(git status --porcelain 2> /dev/null | grep -v '$\?\?' | tail -n1) != "" ]]; then
+if [ $(git status --porcelain 2> /dev/null | grep -v '$\?\?' | tail -n1) ! "" ]; then
   echo "${red}Uncommitted changes would be lost. Commit or stash these changes:${off}"
   git status
   exit 1
@@ -38,7 +37,7 @@ function commit_build_artifacts() {
   git add -f "${build_paths}"
 
   # Commit the build artifacts on the branch.
-  git commit -a -m "Built ${version} on $(date)."  
+  git commit -a -m "Built ${version} on $(date)."
 
   # Always succeed.
   return 0
@@ -54,12 +53,12 @@ if [[ -z $(type -t deploy) ]]; then
 fi
 
 (
-  create_build_branch &&  
+  create_build_branch &&
   echoed yarn &&
   echoed npm run build &&
   commit_build_artifacts &&
   deploy
- 
+
   # Regardless of whether we succeeded or failed, go back to
   # the previous branch.
   git checkout -
