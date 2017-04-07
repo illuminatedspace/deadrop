5d07d77fcfe8736e8fc71791ed054de783fbac9b
Liz <lizkristinaphillips@gmail.com>
2017-04-07 10:50:53 -0400

    removes secrets route

--- server/api.js
+++ server/api.js
@@ -6,10 +6,10 @@ const api = module.exports = require('express').Router()
 //this is: /api
 
 api
-  .get('/heartbeat', (req, res) => res.send({ok: true,}))
+  // .get('/heartbeat', (req, res) => res.send({ok: true,}))
   .use('/auth', require('./auth'))
   .use('/users', require('./users'))
-  .use('/secrets', require('./secrets'))
+  // .use('/secrets', require('./secrets'))
 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
